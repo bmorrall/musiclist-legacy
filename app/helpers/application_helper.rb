25f518b4a1587454b568a6fc4e806744bf601713
bmorrall <ben.morrall@sitepoint.com>
2011-09-05 15:06:28 +1000

    Changed jQuery to be served by Google CDN

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def jquery_cdn
+    cdn_tag = javascript_include_tag('//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js')
+    script_include = javascript_include_tag('jquery').gsub(/(.*)(<)(.*)/, '\1\\x3C\3')    
+    local_tag = "<script>window.jQuery || document.write('" + script_include + "')</script>"
+    "#{cdn_tag}\n#{local_tag}".html_safe
+  end
 end
