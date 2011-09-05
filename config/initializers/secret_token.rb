bf3336260942ceccd269dfa125a1fd3218850d55
bmorrall <ben.morrall@sitepoint.com>
2011-09-05 13:41:35 +1000

    Initial Rails create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Musiclist::Application.config.secret_token = 'e198ea32aac595ac6238fb3d413ca3e9e0783c03efb0307d7fc497237fae4f23d0478d81ebf0fb1ee6d5793d6734fdbf9cd65f6c13528cbf161fa0f33bd67219'
