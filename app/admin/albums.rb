1629cecb527f8340646fc86ce4ee0911451cb34c
Ben Morrall <bemo56@hotmail.com>
2011-10-09 19:36:22 +1100

    Included Artist on Album admin page

--- app/admin/albums.rb
+++ app/admin/albums.rb
@@ -1,3 +1,12 @@
 ActiveAdmin.register Album do
-  
+  index do
+    column :id
+    column :title
+    column :artist do |album|
+      album.artist.name unless album.artist.nil?
+    end
+    column :created_at
+    column :updated_at
+    default_actions
+  end
 end
