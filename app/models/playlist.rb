ac0b535b8bd6bb79f08a0aa2d23b9c36034f3a78
Ben Morrall <bemo56@hotmail.com>
2011-10-09 20:43:45 +1100

    Added Title to playlist params

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,4 +1,10 @@
 class Playlist < ActiveRecord::Base
   has_many :playlist_albums
   has_many :albums, :through => :playlist_albums
+
+  def to_param
+    parameterized = name.gsub("'s", 's')
+    parameterized = parameterized.parameterize
+    "#{id}-#{parameterized}"
+  end
 end
