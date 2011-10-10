49c8aa49fea259111cb4de6cb3fbaf06c3d381da
Ben Morrall <bemo56@hotmail.com>
2011-10-10 20:34:03 +1100

    Added options to sort by album and title

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,6 +1,14 @@
 class PlaylistsController < InheritedResources::Base
   def show
     @playlist = Playlist.find(params[:id])
-    @playlist_albums = @playlist.albums
+    @playlist_albums = @playlist.playlist_albums.includes(:album => [:artist])
+    case params[:order]
+    when 'album'
+      @playlist_albums = @playlist_albums.joins(:album).order('albums.title')
+    when 'artist'
+      @playlist_albums = @playlist_albums.joins(:album => [:artist]).order('artists.name')
+    else
+      @playlist_albums = @playlist_albums.order('`playlist_albums`.`order`')
+    end
   end
 end
