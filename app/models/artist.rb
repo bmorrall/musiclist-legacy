6aca68184a05b80c4c8e747401b87e5b7a467ede
Ben Morrall <bemo56@hotmail.com>
2011-11-06 14:40:41 +1100

    Artist is sorted by name by default

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,6 +1,7 @@
 class Artist < ActiveRecord::Base
   has_many :albums, :dependent => :nullify
   validates_presence_of :name
+  default_scope :order => 'name asc'
 
   def display_name
     display_name = name.split('&').map{ |n| n.split(',', 2).reverse.join(' ') }.join(' & ')
