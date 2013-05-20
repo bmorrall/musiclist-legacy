41100496bad18b89295d7c1bb6dd64caa6ea276b
bmorrall <ben.morrall@sitepoint.com>
2013-05-20 15:48:57 +1000

    Model specs work with latest factory girl

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -3,19 +3,19 @@ require 'spec_helper'
 describe Artist do
   describe '#display_name' do
     it 'displays "Rolling Stones" as "Rolling Stones"' do
-      artist = Factory.build(:artist, :name => 'Rolling Stones')
+      artist = FactoryGirl.build(:artist, :name => 'Rolling Stones')
       artist.display_name.should == 'Rolling Stones'
     end
     it 'displays "Beatles, The" as "The Beatles"' do
-      artist = Factory.build(:artist, :name => 'Beatles, The')
+      artist = FactoryGirl.build(:artist, :name => 'Beatles, The')
       artist.display_name.should == 'The Beatles'
     end
     it 'displays "Springsteen, Bruce" as "Bruce Springsteen"' do
-      artist = Factory.build(:artist, :name => 'Springsteen, Bruce')
+      artist = FactoryGirl.build(:artist, :name => 'Springsteen, Bruce')
       artist.display_name.should == 'Bruce Springsteen'
     end
     it 'displays "Pop, Iggy & Stooges" as "Iggy Pop & Stooges"' do
-      artist = Factory.build(:artist, :name => 'Pop, Iggy & Stooges')
+      artist = FactoryGirl.build(:artist, :name => 'Pop, Iggy & Stooges')
       artist.display_name.should == 'Iggy Pop & Stooges'
     end
   end
