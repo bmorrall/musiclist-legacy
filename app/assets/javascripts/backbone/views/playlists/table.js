5cecba5eab8a29fc84f34ac17c7384a382f614d5
Ben Morrall <bemo56@hotmail.com>
2013-05-25 01:47:45 +1000

    Added sort comparator with JS

--- app/assets/javascripts/backbone/views/playlists/table.js
+++ app/assets/javascripts/backbone/views/playlists/table.js
@@ -1,7 +1,40 @@
 Musiclist.Views.PlaylistTable = Backbone.View.extend({
 
+  events: {
+    'click a.order-album': 'orderAlbum',
+    'click a.order-artist': 'orderArtist',
+    'click a.order-position': 'orderPosition'
+  },
+
   initialize: function() {
     this.model.on('reset', this.renderAlbums, this);
+  },
+
+  orderAlbum: function(e) {
+    e.preventDefault();
+
+    this.model.comparator = function(album) {
+      return album.get("title").toLowerCase();
+    };
+    this.model.sort();
+  },
+
+  orderArtist: function(e) {
+    e.preventDefault();
+
+    this.model.comparator = function(album) {
+      return album.get("artist").toLowerCase();
+    };
+    this.model.sort();
+  },
+
+  orderPosition: function(e) {
+    e.preventDefault();
+
+    this.model.comparator = function(album) {
+      return album.get("position");
+    };
+    this.model.sort();
   },
 
   render: function() {
@@ -11,12 +44,11 @@ Musiclist.Views.PlaylistTable = Backbone.View.extend({
 
   renderAlbums: function() {
     var _this = this;
-    this.$el.empty();
+    this.$('tbody').empty();
     this.model.each(function(album) {
       var albumView = new Musiclist.Views.PlaylistAlbum({ model: album });
       var rendered = albumView.render();
-      console.log(rendered);
-      _this.$el.append(rendered.el);
+      _this.$('tbody').append(rendered.el);
     })
     return this;
   }
