0a19a5c2e4b5c8aaf79c72af7c39618e5ba6ff95
bmorrall <ben.morrall@sitepoint.com>
2013-05-24 15:31:06 +1000

    Fixed broken request spec

--- spec/requests/album_statuses_spec.rb
+++ spec/requests/album_statuses_spec.rb
@@ -5,7 +5,9 @@ describe "AlbumStatuses" do
   # This should return the minimal set of attributes required to create a valid
   # AlbumStatus.
   def valid_create_attributes
-    FactoryGirl.attributes_for(:album_status)
+    attributes = FactoryGirl.attributes_for(:album_status)
+    attributes[:album_id] = FactoryGirl.create(:album).id
+    attributes
   end
 
   describe "POST /album_statuses.json" do
@@ -13,7 +15,7 @@ describe "AlbumStatuses" do
       before(:each) { sign_in_user }
       it "creates a new Album status and returns a created status" do
         post album_statuses_url(:format => :json), :album_status => valid_create_attributes
-        response.status.should eq(204)
+        response.status.should eq(201) # Created
       end
     end
   end
