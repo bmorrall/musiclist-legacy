ab3aedc500a01030ed3e3f9c92dbcfeb01fdef6f
Ben Morrall <bemo56@hotmail.com>
2013-05-25 11:52:30 +1000

    Added js table filters

--- app/assets/javascripts/backbone/views/playlists/table.js
+++ app/assets/javascripts/backbone/views/playlists/table.js
@@ -3,38 +3,44 @@ Musiclist.Views.PlaylistTable = Backbone.View.extend({
   events: {
     'click a.order-album': 'orderAlbum',
     'click a.order-artist': 'orderArtist',
-    'click a.order-position': 'orderPosition'
+    'click a.order-position': 'orderPosition',
+    'change #album-played, #album-purchased, #album-unheard': 'changeFilter'
   },
 
   initialize: function() {
-    this.model.on('reset', this.renderAlbums, this);
+    this.filteredModels = new Musiclist.Models.Albums();
+    this.model.on('reset', this.updateFilter, this);
+  },
+
+  changeFilter: function(e) {
+    this.updateFilter();
   },
 
   orderAlbum: function(e) {
     e.preventDefault();
 
-    this.model.comparator = function(album) {
+    this.filteredModels.comparator = function(album) {
       return album.get("title").toLowerCase();
     };
-    this.model.sort();
+    this.filteredModels.sort();
   },
 
   orderArtist: function(e) {
     e.preventDefault();
 
-    this.model.comparator = function(album) {
+    this.filteredModels.comparator = function(album) {
       return album.get("artist").toLowerCase();
     };
-    this.model.sort();
+    this.filteredModels.sort();
   },
 
   orderPosition: function(e) {
     e.preventDefault();
 
-    this.model.comparator = function(album) {
+    this.filteredModels.comparator = function(album) {
       return album.get("position");
     };
-    this.model.sort();
+    this.filteredModels.sort();
   },
 
   render: function() {
@@ -45,12 +51,37 @@ Musiclist.Views.PlaylistTable = Backbone.View.extend({
   renderAlbums: function() {
     var _this = this;
     this.$('tbody').empty();
-    this.model.each(function(album) {
+    
+    console.debug(this.filteredModels.length);
+    this.filteredModels.each(function(album) {
       var albumView = new Musiclist.Views.PlaylistAlbum({ model: album });
       var rendered = albumView.render();
       _this.$('tbody').append(rendered.el);
     })
     return this;
+  },
+
+  updateFilter: function() {
+    var filterPlayed = this.$('#album-played').is(':checked');
+    var filterPurchased = this.$('#album-purchased').is(':checked');
+    var filterUnheard = this.$('#album-unheard').is(':checked');
+    this.filteredModels.reset(this.model.filter(function(album) {
+      var display = false;
+
+      if (filterPlayed) {
+        display = display || album.get('played');
+      }
+      if (filterPurchased) {
+        display = display || album.get('purchased');
+      }
+      if (filterUnheard) {
+        display = display || (!album.get('played') && !album.get('purchased'))
+      }
+
+      return display;
+    }));
+
+    this.renderAlbums();
   }
 
 });
